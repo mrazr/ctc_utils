ace95de8c1d66896d932566fbe9e3f6f0414e8d0
Radoslav Mraz <radoslav.mraz95@gmail.com>
2022-06-21 21:25:38 +0200

    added param for data_format for Conv2D

--- transBTS.py
+++ transBTS.py
@@ -42,28 +42,30 @@ def mlp(x, hidden_units, dropout_rate):
     return x
 
 
-def create(image_size: typing.Tuple[int, int]):
+def create(image_size: typing.Tuple[int, int], data_format: str = 'channels_last'):
     input = keras.layers.Input(shape=image_size)  # (128, 128, 1)
 
-    x = keras.layers.Conv2D(activation="relu", filters=4, kernel_size=3, padding="same")(input)  # (128, 128, 4)
+    x = keras.layers.Conv2D(data_format=data_format, filters=4, kernel_size=3, padding="same")(input)  # (128, 128, 4)
     x = keras.layers.Dropout(0.1)(x)  # (128, 128, 4)
 
     en1 = enblock(x, 4)  # (128, 128, 4)
 
-    x = keras.layers.Conv2D(activation="relu", filters=8, kernel_size=3, strides=(2, 2), padding="same")(en1)  # (64, 64, 8)
+    x = keras.layers.Conv2D(data_format=data_format, filters=8, kernel_size=3, strides=(2, 2), padding="same")(en1)  # (64, 64, 8)
 
     en2 = enblock(enblock(x, 8), 8)  # (64, 64, 8)
 
-    x = keras.layers.Conv2D(activation="relu", filters=16, kernel_size=3, strides=(2, 2), padding="same")(en2)  # (32, 32, 16)
+    x = keras.layers.Conv2D(data_format=data_format, filters=16, kernel_size=3, strides=(2, 2), padding="same")(en2)  # (32, 32, 16)
 
     en3 = enblock(enblock(x, 16), 16)  # (32, 32, 16)
 
-    x = keras.layers.Conv2D(activation="relu", filters=32, kernel_size=3, strides=(2, 2), padding="same")(en3)  # (16, 16, 32)
+    x = keras.layers.Conv2D(data_format=data_format, filters=32, kernel_size=3, strides=(2, 2), padding="same")(en3)  # (16, 16, 32)
 
     en4 = enblock(enblock(x, 32), 32)  # (16, 16, 32)
 
-    lin_proj = keras.layers.Conv2D(activation="relu", filters=128, kernel_size=3, padding="same")(en4)  # (16, 16, 128)
-    lin_proj = keras.layers.Permute((3, 1, 2))(lin_proj)  # (128, 16, 16)
+    lin_proj = keras.layers.Conv2D(data_format=data_format, filters=128, kernel_size=3, padding="same")(en4)  # (16, 16, 128)
+    if data_format == "channels_last":
+        lin_proj = keras.layers.Permute((3, 1, 2))(lin_proj)  # (128, 16, 16)
+
     lin_proj = keras.layers.Reshape((128, 256))(lin_proj)  # (128, 256)
 
     x = transformer_block(lin_proj)
@@ -71,37 +73,38 @@ def create(image_size: typing.Tuple[int, int]):
     x = transformer_block(x)
     x = transformer_block(x)  # (128, 256)
 
-    x = keras.layers.Permute((2, 1))(x)  # (256, 128)
+    if data_format == "channels_last":
+        x = keras.layers.Permute((2, 1))(x)  # (256, 128)
     x = keras.layers.Reshape((16, 16, -1))(x)  # (16, 16, 128)
 
-    feat_map = keras.layers.Conv2D(activation="relu", filters=64, kernel_size=1, padding="same")(x)  # (16, 16, 64)
-    feat_map = keras.layers.Conv2D(activation="relu", filters=32, kernel_size=1, padding="same")(feat_map)  # (16, 16, 32)
+    feat_map = keras.layers.Conv2D(data_format=data_format, filters=64, kernel_size=1, padding="same")(x)  # (16, 16, 64)
+    feat_map = keras.layers.Conv2D(data_format=data_format, filters=32, kernel_size=1, padding="same")(feat_map)  # (16, 16, 32)
 
     x = keras.layers.Concatenate(axis=-1)([feat_map, en4])  # (16, 16, 64)
     x = enblock(enblock(x, 64), 64)  # (16, 16, 64)
 
-    x = keras.layers.Conv2D(activation="relu", filters=64, kernel_size=3, padding="same")(x)  # (16, 16, 64)
+    x = keras.layers.Conv2D(data_format=data_format, filters=64, kernel_size=3, padding="same")(x)  # (16, 16, 64)
     x = keras.layers.Conv2DTranspose(filters=64, kernel_size=3, padding="same", strides=2)(x)  # (32, 32, 64)
-    x = keras.layers.Conv2D(activation="relu", filters=16, kernel_size=1, padding="same")(x)  # (32, 32, 16)
+    x = keras.layers.Conv2D(data_format=data_format, filters=16, kernel_size=1, padding="same")(x)  # (32, 32, 16)
 
     x = keras.layers.Concatenate(axis=-1)([x, en3])  # (32, 32, 32)
     x = enblock(x, 32)  # (32, 32, 32)
 
-    x = keras.layers.Conv2D(activation="relu", filters=32, kernel_size=3, padding="same")(x)  # (32, 32, 32)
+    x = keras.layers.Conv2D(data_format=data_format, filters=32, kernel_size=3, padding="same")(x)  # (32, 32, 32)
     x = keras.layers.Conv2DTranspose(filters=32, kernel_size=3, padding="same", strides=2)(x)  # (64, 64, 32)
-    x = keras.layers.Conv2D(activation="relu", filters=8, kernel_size=1, padding="same")(x)  # (64, 64, 8)
+    x = keras.layers.Conv2D(data_format=data_format, filters=8, kernel_size=1, padding="same")(x)  # (64, 64, 8)
 
     x = keras.layers.Concatenate(axis=-1)([x, en2])  # (64, 64, 16)
     x = enblock(x, 16)  # (64, 64, 16)
 
-    x = keras.layers.Conv2D(activation="relu", filters=16, kernel_size=3, padding="same")(x)  # (64, 64, 16)
+    x = keras.layers.Conv2D(data_format=data_format, filters=16, kernel_size=3, padding="same")(x)  # (64, 64, 16)
     x = keras.layers.Conv2DTranspose(filters=16, kernel_size=3, padding="same", strides=2)(x)  # (128, 128, 16)
-    x = keras.layers.Conv2D(activation="relu", filters=4, kernel_size=1, padding="same")(x)  # (128, 128, 4)
+    x = keras.layers.Conv2D(data_format=data_format, filters=4, kernel_size=1, padding="same")(x)  # (128, 128, 4)
 
     x = keras.layers.Concatenate(axis=-1)([x, en1])  # (128, 128, 8)
     x = enblock(x, 8)  # (128, 128, 8)
 
-    out = keras.layers.Conv2D(filters=1, kernel_size=1, padding="same",
+    out = keras.layers.Conv2D(data_format=data_format, filters=1, kernel_size=1, padding="same",
                               activation="softmax")(x)  # (128, 128, 1)
 
     return keras.Model(inputs=input, outputs=out)
